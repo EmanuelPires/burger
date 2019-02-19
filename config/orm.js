701fd0a75ec591593d48020364b1a03ec793d4a3
Emanuel Pires <emanuelpires11@gmail.com>
2019-02-19 15:59:21 -0500

    folders complete

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,33 @@
 var connection = require("./connection.js");
 
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
 var orm = {
-  selectAll: function() {},
-  insertOne: function() {},
-  updateOne: function() {}
+  selectAll: function() {
+    var query = "SELECT * FROM burgers";
+    connection.query(query, function(err, data) {
+      if (err) throw err;
+      res.json(data);
+    });
+  },
+  insertOne: function(table, cols, vals, cb) {
+    var queryString = "ISERT INTO " + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+  },
+  updateOne: function(id, devoured) {}
 };
+
+module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(data) {
+      cb(data);
+    });
+  },
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(data) {
+      cb(data);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(data) {
+      cb(data);
+    });
+  }
+};
+
+module.exports = burger;
